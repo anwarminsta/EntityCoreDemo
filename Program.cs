b79bb73c81d239fd16dcb08724e699032ee94e8a
Md Anwar <anwar.minst1@gmail.com>
2025-12-28 17:46:21 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+namespace EntityCoreDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+            
+            // Default endpoint
+            app.MapGet("/", () => "Welcome to EntityCoreDemo API");
+
+            app.Run();
+        }
+    }
+}
